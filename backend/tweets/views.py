5b5a1014d49ee6846001d4427a8cf448a2fcceaa
SushiRocket <kentamori27@gmail.com>
2024-12-25 07:08:33 +0000

    Create TweetListCreateView

--- backend/tweets/views.py
+++ backend/tweets/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from.models import Tweet
+from.serializers import TweetSerializers
 
 # Create your views here.
+
+class TweetListCreateView(generics.ListCreateAPIView):
+    queryset = Tweet.objects.all().order_by('-created_at')
+    serializer_class = TweetSerializers
