e6d0306de0ad4e0ca8a4acaf4fd0585e2c3ec250
SushiRocket <kentamori27@gmail.com>
2024-12-25 09:42:26 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
 // frontend/src/App.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import TweetCreateForm from './components/TweetCreateForm';
 import TweetList from './components/Tweetlist';
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+
+  // 初回レンダリング時に既存ツイート一覧を取得
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+
+  const fetchTweets = () => {
+    axios.get('http://localhost:8000/api/tweets/')
+      .then(response => {
+        setTweets(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching tweets:', error);
+      });
+  };
+  const handleTweetCreated = (newTweet) => {
+    setTweets([newTweet, ...tweets]);
+  };
+
   return (
-    <div className="App">
-      <TweetList />
+    <div>
+      <h1>Tweet App</h1>
+      {/* 新規投稿フォーム：投稿完了時に handleTweetCreated が呼ばれる */}
+      <TweetCreateForm onTweetCreated={handleTweetCreated} />
+      <TweetList tweets={tweets} />
     </div>
   );
 }
