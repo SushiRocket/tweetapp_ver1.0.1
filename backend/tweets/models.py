5f8a05a7dabf64fc02114f347bfcb1322484c5e3
SushiRocket <kentamori27@gmail.com>
2024-12-25 06:55:36 +0000

    Create Tweet model

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -1,3 +1,10 @@
+# backend/tweets/models.py
+
 from django.db import models
 
-# Create your models here.
+class Tweet(models.Model):
+    text = models.CharField(max_length=280)   # ツイート本文(280文字まで)
+    created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
+
+    def __str__(self):
+        return self.text[:20]  # 管理画面などで表示するとき用
