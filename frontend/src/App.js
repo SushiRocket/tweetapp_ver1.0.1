446284e1601f89df23e3c649ac5a34a3d4914c53
SushiRocket <kentamori27@gmail.com>
2024-12-25 08:37:27 +0000

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// frontend/src/App.js
+import React from 'react';
+import TweetList from './components/Tweetlist';
+
+function App() {
+  return (
+    <div className="App">
+      <TweetList />
+    </div>
+  );
+}
+
+export default App;
