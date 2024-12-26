4fd728d66c8ca8ba401167329151c4f45ab54706
SushiRocket <kentamori27@gmail.com>
2024-12-26 08:15:16 +0000

    Start App Users

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt"
+    "users",
 ]
 
 RESt_FRAMEWORK = {
---
+++ backend/users/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from.serializers import UserSerializer
+
+# Create your views here.
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
