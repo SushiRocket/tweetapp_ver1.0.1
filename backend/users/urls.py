6a38ba07a5d6adc0df6348aea07c58d1298219b8
SushiRocket <kentamori27@gmail.com>
2024-12-27 06:50:33 +0000

    Add Path For UcerDetailView

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from.views import UserCreateView
+from.views import UserCreateView, UserDtailView
 
 urlpatterns = [
-    path('register', UserCreateView.as_view(), name='user_register')
+    path('register', UserCreateView.as_view(), name='user_register'),
+    path('user/', UserDtailView.as_view(), name='user_detail'),
 ]
