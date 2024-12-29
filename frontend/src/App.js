bf6af844d94c28a365a2271d0beb13e94491f3ed
SushiRocket <kentamori27@gmail.com>
2024-12-29 02:16:25 +0000

    Add Login and Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 // frontend/src/App.js
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider, AuthContext } from './Contexts/AuthContext';
+import Login from './components/Login';
+import Register from './components/Register';
 import axios from 'axios';
 import TweetCreateForm from './components/TweetCreateForm';
 import TweetList from './components/Tweetlist';
@@ -30,13 +34,48 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Tweet App</h1>
-      <TweetCreateForm onTweetCreated={handleTweetCreated} />
-      {/* TweetList には tweets を props で渡すだけ */}
-      <TweetList tweets={tweets} />
-    </div>
+    <><AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/regidter" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </AuthProvider><div>
+        <h1>Tweet App</h1>
+        <TweetCreateForm onTweetCreated={handleTweetCreated} />
+        {/* TweetList には tweets を props で渡すだけ */}
+        <TweetList tweets={tweets} />
+      </div></>
   );
 }
+
+  const Home = () => {
+    const { user } = React.useContext(AuthContext);
+
+    return (
+      <div>
+        <h1>Tweet App</h1>
+        { user ? (
+          <>
+            <TweetCreateForm />
+            <TweetLitst />
+            <LOgoutButton />
+          </>
+        ) : (
+          <Navigate to ="/login" />
+        )}
+      </div>
+    );
+  };
+
+  const LogoutButton = () => {
+    const { logout } = React.useContext(AuthContext);
+
+    return (
+      <button onClick={logout}>Logout</button>
+    );
+  };
 
 export default App;
