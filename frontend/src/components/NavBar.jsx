59255541dfc0748fb89a1a2335a8a8393a3bde9b
SushiRocket <kentamori27@gmail.com>
2025-01-07 09:58:52 +0000

    Create NavBar.jsx

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+
+const NavBar = () => {
+    const { user, logout } =useContext(AuthContext);
+
+    return (
+        <nav>
+            <h2>Tweet App</h2>
+            <div>
+                {user ? (
+                    <>
+                        <span>Welcome, {user.username}!</span>
+                        <button OnClick={logout}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <Link to='/login'>Login</Link>                       
+                        <Link to='/register'>Register</Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
