6c303951db25e0b573aa8577d317e371e93dfcbc
SushiRocket <kentamori27@gmail.com>
2025-01-09 05:50:17 +0000

    パーミッションをビューにセット

--- backend/tweets/views.py
+++ backend/tweets/views.py
@@ -4,13 +4,15 @@ from rest_framework import viewsets
 from.models import Tweet
 from.serializers import TweetSerializers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
 class TweetViewSet(viewsets.ModelViewSet):
     queryset = Tweet.objects.all().order_by('-created_at')
     serializer_class = TweetSerializers
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]]
+    
     #ツイートが作成される際に、シリアライザーに現在のリクエストユーザー (self.request.user) を自動的に設定します。
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
