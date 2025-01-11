5e219acdb02a48eeae246afdc86d9c22fd54e015
SushiRocket <kentamori27@gmail.com>
2025-01-11 11:00:57 +0000

    Create Home.jsx

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../contexrts/AuthContext';
+import TweetCreateForm from './TweetCreateForm';
+import TweetList from './TweetList';
+import LogoutButton from './LogoutButton';
+
+const Home = () => {
+    const { user } = useContext(AuthContext);
+
+    return (
+        <div>
+            <h1>Tweet App</h1>
+            {user ? (
+                <>
+                <TweetCreateForm />
+                <TweetList />
+                <LogoutButton />
+                </>
+            ) : ( 
+                <p>Loding...</p>
+            )}
+        </div>
+    );
+};
+
+export default Home;
