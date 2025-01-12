f73defbf61206c3bee745632ab715dd99aae6e7c
SushiRocket <kentamori27@gmail.com>
2025-01-12 03:55:03 +0000

    noting to change

--- backend/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    following = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='followers'
-    )
