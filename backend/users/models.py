42da935dc7b08e378d9abb9c8cd2663d17d94f65
SushiRocket <kentamori27@gmail.com>
2025-01-12 03:34:56 +0000

    kカスタムユーザーを作成。フォロー、フォロワーを追加

---
+++ backend/users/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers'
+    )
