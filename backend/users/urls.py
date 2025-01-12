f41994573c92f9bd3faa46fa3f92d5bd3ea110d1
SushiRocket <kentamori27@gmail.com>
2025-01-12 05:51:03 +0000

    フォロー機能用のエンドポイントの設定

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 # backend/users/urls.py
 
 from django.urls import path
-from.views import UserCreateView, UserDetailView
+from.views import UserCreateView, UserDetailView,FollowAPIView
 
 urlpatterns = [
     path('register/', UserCreateView.as_view(), name='user_register'),
     path('user/', UserDetailView.as_view(), name='user_detail'),
+    path('user/<int:pk>/follow/', FollowAPIView.as_view(), name='user-follow'),
+    path('user/<int:pk>/unfollow/', FollowAPIView.as_view(), name='user-unfollow'),
 ]
