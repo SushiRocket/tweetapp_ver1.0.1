f7b77acf0e091438a03e6519bb8ec20acea11f1b
SushiRocket <kentamori27@gmail.com>
2025-01-12 04:35:25 +0000

    Create FollowSerializerの追記

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,15 +2,29 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from.models import Follow
 
 # Create your models here.
 
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = ('id', 'follower', 'following', 'created_at')
+
 class UserSerializer(serializers.ModelSerializer):
+    followers = serializers.SerializerMethodField()
+    following = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id','username','password','email','first_name','last_name')
+        fields = ('id','username','password','email','first_name','last_name', 'followers', 'following')
+
+    def get_followers(self, obj):
+        return obj.followers.count()
+    
+    def get_following(self, obj):
+        return obj.followoing.count()
 
     def create(self, validated_data):
         user = User.objects.create_user(
