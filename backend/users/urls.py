ce36e29f959e866e6ec236b259c369541337ad19
SushiRocket <kentamori27@gmail.com>
2025-01-15 13:41:32 +0000

    UserViewSetのルーティングを設定

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,14 @@
 # backend/users/urls.py
 
-from django.urls import path
-from.views import UserCreateView, UserDetailView,FollowAPIView
+from django.urls import path,include
+from rest_framework import routers
+from.views import UserCreateView, UserDetailView,FollowAPIView,UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register/', UserCreateView.as_view(), name='user_register'),
     path('user/', UserDetailView.as_view(), name='user_detail'),
     path('user/<int:pk>/follow/', FollowAPIView.as_view(), name='user-follow'),
