8c8d316cd332798d6ad9bf9b5dedb6cfe915be1d
SushiRocket <kentamori27@gmail.com>
2025-01-16 01:06:19 +0000

    FeedViewを作成してフォロー中のユーザーのツイートを取得する設定

--- backend/tweets/views.py
+++ backend/tweets/views.py
@@ -5,6 +5,8 @@ from.models import Tweet
 from.serializers import TweetSerializers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from.permissions import IsOwnerOrReadOnly
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -16,3 +18,16 @@ class TweetViewSet(viewsets.ModelViewSet):
     #ツイートが作成される際に、シリアライザーに現在のリクエストユーザー (self.request.user) を自動的に設定します。
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class FeedView(APIView):
+
+    # フォローしているユーザーのツイートを取得
+
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get(self, request):
+        user = request.user
+        following_users = user.user.following.value_list('following', flat=True)
+        tweets = Tweet.objects.filter(user__id__in=following_users).order_by('-created_at')
+        serializer = TweetSerializers(tweets, many=True)
+        return Response(serializer.date)
