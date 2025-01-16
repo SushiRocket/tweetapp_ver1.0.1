51b54e106d6660309df77fedf5ad867fb176470b
SushiRocket <kentamori27@gmail.com>
2025-01-16 04:20:57 +0000

    ビュー通知のエンドポイントの設定

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('tweets.urls')),
     path('api/', include('users.urls')),
+    path('api/', include('notifications.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
