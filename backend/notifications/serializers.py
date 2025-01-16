3b2cfce1dd86952450dbaf00128048f6f4f9f956
SushiRocket <kentamori27@gmail.com>
2025-01-16 05:01:06 +0000

    Fix spelling mistake

--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from.models import Notification
 
-class NotificationSerializer(serializers.Modelserializer):
+class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         field = ['id', 'recipient', 'sender', 'message', 'created_at', 'is_read']
