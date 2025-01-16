2140a166085954d7145a1c7d43d26ab676b39416
SushiRocket <kentamori27@gmail.com>
2025-01-16 04:03:28 +0000

    通知一覧ビューと既読、未読のビューを設定

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from.models import Notification
+from.serializers import NotificationSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class NotificationListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        notifications = Notification.objects.filter(recipient=request.user, is_read=False)
+        serializer = NotificationSerializer(notifications, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        notifications = Notification.objects.filter(recipient=request.user, is_read=False)
+        notifications.update(is_read=True)
+        return Response({'status': 'Notifications marked as read'})
