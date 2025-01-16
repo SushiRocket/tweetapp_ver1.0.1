af454d9f046ab9c76ed36bb049090cac6867484f
SushiRocket <kentamori27@gmail.com>
2025-01-16 01:18:50 +0000

    FeedViewのエンドポイント作成。拡張性があるAPIViewを使用。

--- backend/tweets/urls.py
+++ backend/tweets/urls.py
@@ -2,11 +2,12 @@
 
 from django.urls import path,include
 from rest_framework import routers
-from.views import TweetViewSet
+from.views import TweetViewSet,FeedView
 
 router = routers.DefaultRouter()
 router.register('tweets', TweetViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
