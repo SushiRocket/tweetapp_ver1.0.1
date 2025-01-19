be342c4ee4ed4e1653af91571b4b26698cbb7963
SushiRocket <kentamori27@gmail.com>
2025-01-19 11:52:06 +0000

    ツイート時にハッシュタグを登録。

--- backend/tweets/views.py
+++ backend/tweets/views.py
@@ -1,7 +1,7 @@
 #backend/tweets/views.py
 
 from rest_framework import viewsets,status
-from.models import Tweet,Comment,Like
+from.models import Tweet,Comment,Like,Hashtag
 from.serializers import TweetSerializer,CommentSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 from.permissions import IsOwnerOrReadOnly
@@ -9,9 +9,39 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-
+from .utils import extract_and_link_hashtags
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
+class HashtagDetailAPIView(APIView):
+    # 指定ハッシュタグに紐づくツイート一覧を返す
+    # GET /api/hashtags/<tag_name>/
+    def get(self, request, tag_name):
+        normalized_name = tag_name.lower()
+        hashtag = get_object_or_404(Hashtag, name=normalized_name)
+        tweets = hashtag.tweets.order_by('-created_at')
+        serializer = TweetSerializer(tweets, many=True)
+        return Response({
+            'hashtag': hashtag.name,
+            'tweet_count': tweets.count(),
+            'tweets': serializer.data
+        })
+
+class TrendingHashtagsAPIView(APIView):
+    def get(self, request):
+        hashtags = Hashtag.objects.annotate(
+            tweet_count=Count('tweets')
+        ).order_by('-tweet_count')[:10]
+
+        data = []
+        for tag in hashtags:
+            data.append({
+                'name': tag.name,
+                'tweet_count': tag.tweets.count(),
+            })
+        return Response(data)
+    
 class TweetViewSet(viewsets.ModelViewSet):
     queryset = Tweet.objects.all().order_by('-created_at')
     serializer_class = TweetSerializer
@@ -20,6 +50,8 @@ class TweetViewSet(viewsets.ModelViewSet):
     #ツイートが作成される際に、シリアライザーに現在のリクエストユーザー (self.request.user) を自動的に設定します。
     def perform_create(self, serializer):
         tweet = serializer.save(user=self.request.user)
+        #ハッシュタグの抽出・連携
+        extract_and_link_hashtags(tweet)
         # WebSocket に通知
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
