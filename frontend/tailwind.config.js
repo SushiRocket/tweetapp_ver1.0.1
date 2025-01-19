4b91387142755c4f92dadeb1e99919bb2992be5f
SushiRocket <kentamori27@gmail.com>
2025-01-19 07:00:25 +0000

    extendを追加してテーマカラーを自由に拡張

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: '#3abff8',
+          DEFAULT: '#0ea5e9',
+          dark: '#0284c7'
+        }
+      },
+    },
   },
   plugins: [],
 }
