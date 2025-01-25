a06b8da9b0f24f603e8cc149d81be11466c025b3
SushiRocket <kentamori27@gmail.com>
2025-01-25 05:30:08 +0000

    Create model for DM.

--- backend/dm/models.py
+++ backend/dm/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class DirectMessage(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
-    Content = models.TextField(max_length=1000)
+    content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
