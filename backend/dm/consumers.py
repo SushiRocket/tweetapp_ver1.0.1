6ec813bd8ce8462fc56e4502a151f322ce8ebfac
SushiRocket <kentamori27@gmail.com>
2025-01-25 05:28:15 +0000

    Create DMConsumer.

--- backend/dm/consumers.py
+++ backend/dm/consumers.py
@@ -14,7 +14,10 @@ class DMConsumer(AsyncWebsocketConsumer):
         #urlに相手ユーザーのIDが含まれる想定
         self.other_user_id = self.scope['url_route']['kwargs']['user_id']
         self.user = self.scope['user']
-        if self.user.is_anonymas:
+        print(f"WebSocket connected. User: {self.scope['user']}, Is Authenticated: {self.scope['user'].is_authenticated}")
+    
+
+        if self.user.is_anonymous:
             await self.close()
 
         else:
@@ -28,19 +31,22 @@ class DMConsumer(AsyncWebsocketConsumer):
             )
             await self.accept()
 
-        async def disconect(self, close_code):
-            await self.channel_layer.group_discard(
-                self.room_name,
-                self.channel_name
-            )
+    async def disconect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_name,
+            self.channel_name
+        )
 
-        async def receive(self, content_data):
-            data = json.loads(content_data)
+    async def receive(self, text_data=None, bytes_data=None):
+        
+        # Channels が内部で「text_data=...」という形で呼び出すので、
+        # その引数を受け取れるようにする。
+        if text_data is not None:
+            print(">>>[DMConsumer] recieve() called with", text_data)
+            data = json.loads(text_data)
             message_content = data.get('message')
-
             # メッセージをDBに保存
             dm_id = await self.save_dm(message_content)
-
             # グループにブロードキャスト
             await self.channel_layer.group_send(
                 self.room_name,
@@ -52,22 +58,24 @@ class DMConsumer(AsyncWebsocketConsumer):
                 }
             )
 
-        async def db_message(self, event):
-            # クライアントにメッセージを送信
-            dm_data = {
-                'sender_id': event['sender_id'],
-                'dm_id': event['dm_id'],
-                'text': event['content'],
-            }
-            await self.send(content_data=json.dumps(dm_data))
+    async def dm_message(self, event):
+        print(">>>[DMConsumer] dm_message() broadcast", event)
+        # クライアントにメッセージを送信
+        dm_data = {
+            'sender_id': event['sender_id'],
+            'dm_id': event['dm_id'],
+            'content': event['content'],
+        }
+        await self.send(json.dumps(dm_data))
 
-        @sync_to_async
-        def save_dm(self, content):
-            #DBに保存
-            recipient_id = self.other_user_id
-            dm = DirectMessage.objects.create(
-                sender_id=self.user.id,
-                recipient_id = recipient_id,
-                content = content
-            )
-            return dm.id
+    @sync_to_async
+    def save_dm(self, content):
+        #DBに保存
+        recipient_id = self.other_user_id
+        print(f"Saving DM: {self.user.id} -> {recipient_id}, content={content}")
+        dm = DirectMessage.objects.create(
+            sender_id=self.user.id,
+            recipient_id = recipient_id,
+            content = content
+        )
+        return dm.id
